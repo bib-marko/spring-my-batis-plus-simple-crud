752fb4c9141d3729a7a7399e884beea846969a11
MarkO <olivaresmarkanthony14@outlook.com>
2022-09-08 13:53:08 +0800

    init

--- src/main/java/com/demomybatisplus/mybatisplus/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.demomybatisplus.mybatisplus.exception;
-
-    public class UserAlreadyExistException extends RuntimeException{
-        public UserAlreadyExistException(String message) {
-            super(message);
-        }
-    }
--- src/main/java/com/demomybatisplus/mybatisplus/exception/UserNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.demomybatisplus.mybatisplus.exception;
-
-public class UserNotExistException extends RuntimeException{
-    public UserNotExistException(String message) {
-        super(message);
-    }
-}
